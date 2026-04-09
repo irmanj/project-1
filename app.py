d0acc312ad9b4d9697638f3a6ad758039af220fb
irmanj <irma.nurjayanti@gmail.com>
2026-04-09 17:15:10 +0700

    fixing

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ st.title("🎓 Prediksi Kelulusan Siswa")
 jam = st.slider("Jam belajar", 0, 10)
 hadir = st.slider("Kehadiran", 0, 100)
 
+st.write(f"Jam belajar: {jam} jam")
+st.write(f"Kehadiran: {hadir}%")
+
 if st.button("Prediksi"):
     
     input_data = pd.DataFrame({
@@ -23,3 +26,11 @@ if st.button("Prediksi"):
         st.success("Siswa kemungkinan LULUS ✅")
     else:
         st.error("Siswa kemungkinan TIDAK LULUS ❌")
+
+# confidence
+proba = model.predict_proba(input_data)
+
+st.write(f"Probabilitas Lulus: {proba[0][1]*100:.2f}%")
+
+st.markdown("---")
+st.caption("Model: Random Forest | Dibuat oleh irmanj")
